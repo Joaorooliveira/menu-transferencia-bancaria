1b0d2f93ac19619c13501764158dfa7e6f5a54a7
joaorooliveira <joaovictorooliveira812@gmail.com>
2025-02-20 18:42:35 -0300

    Menu finalizado

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
         System.out.println("Informe seu saldo inicial:");
         double saldoInicial = leitura.nextDouble();
         double saldoAtualizado = saldoInicial;
+        double variavelSuporte =0;
         //Mostrar ao Usuario os seus dados
         System.out.println("***********************");
         System.out.println("Dados iniciais do cliente:");
@@ -38,14 +39,25 @@ public class Main {
 
             switch (opcao){
                 case 1:
-                    System.out.println(saldoAtualizado);
+                    System.out.println("Seu saldo e :"+ saldoAtualizado);
                     break;
                 case 2:
                     System.out.println("Informe o valor a receber:");
-                    saldoAtualizado += leitura.nextDouble();
+                    variavelSuporte = leitura.nextDouble();
+                    saldoAtualizado += variavelSuporte;
+                    System.out.println("Saldo atualizado R$ "+saldoAtualizado);
                     break;
                 case 3:
                     System.out.println("Informe o valor que deseja transferir:");
+                    variavelSuporte = leitura.nextDouble();
+                    System.out.println(variavelSuporte);
+                    if(variavelSuporte<=saldoAtualizado) {
+                        saldoAtualizado = saldoAtualizado-variavelSuporte;
+                        System.out.println("Saldo atualizado R$ " + saldoAtualizado);
+                    }else{
+                        System.out.println("Nao ha saldo suficiente para fazer essa transferencia.");
+                    }
+                    break;
             }
         }while(opcao!=4);
 
